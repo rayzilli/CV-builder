4183770db7407e6a5b439a13954444cfe1ad993b
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2025-05-13 19:54:28 -0400

    Add General to components and App
    Make a new component folder and export it
    to app which gets exported to main.

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import General from './General.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <General />
+    <App />
   </StrictMode>,
 )
+ 
